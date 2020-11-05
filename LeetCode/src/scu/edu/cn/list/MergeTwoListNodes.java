955efc771029dc301cdea997dc0c1277fe5a9cec
zzy0602 <2541488765@qq.com>
2020-11-05 22:47:58 +0800

    加了一个迭代法更简便易读的写法

--- LeetCode/src/scu/edu/cn/list/MergeTwoListNodes.java
+++ LeetCode/src/scu/edu/cn/list/MergeTwoListNodes.java
@@ -80,6 +80,26 @@ public class MergeTwoListNodes {
         }
 
     }
+//  迭代法
+    public ListNode mergeTwoLists2(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
 
 
     public static void main(String[] args) {
