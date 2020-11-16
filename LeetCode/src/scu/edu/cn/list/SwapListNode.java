35d325002e967b5fe44404f4396ed5c59f58dfc1
zzy0602 <2541488765@qq.com>
2020-11-16 23:32:44 +0800

    题目补充

--- LeetCode/src/scu/edu/cn/list/SwapListNode.java
+++ LeetCode/src/scu/edu/cn/list/SwapListNode.java
@@ -3,6 +3,10 @@ package scu.edu.cn.list;
 import java.util.ArrayList;
 import java.util.List;
 
+//
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//
+//        你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 public class SwapListNode {
     public ListNode swapPairs(ListNode head) {
         ListNode listNode = head;
